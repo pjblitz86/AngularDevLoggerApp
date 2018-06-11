07710e13fe39cced5b34513fd67ca8a77e2eb460
Paulius Juknis <paulius.juknis@gmail.com>
2018-06-11 16:08:45 +0300

    implemented add, update and delete logs

--- src/app/components/log-form/log-form.component.ts
+++ src/app/components/log-form/log-form.component.ts
@@ -12,6 +12,9 @@ export class LogFormComponent implements OnInit {
   text: string;
   date: any;
 
+  isNew: boolean = true;
+
+
   constructor(private logService: LogService) { }
 
   ngOnInit() {
@@ -19,11 +22,43 @@ export class LogFormComponent implements OnInit {
     this.logService.selectedLog
       .subscribe(log => {
         if (log.id !== null) {
+          this.isNew = false;
           this.id = log.id;
           this.text = log.text;
           this.date = log.date;
         }
       });
-
   }
+
+  onSubmit() {
+    // check if new log
+    if (this.isNew) {
+      // create a new log
+      const newLog = {
+        id: this.generateId(),
+        text: this.text,
+        date: new Date()
+      };
+      // Add log
+      this.logService.addLog(newLog);
+    } else {
+      // create log to be updated
+      const updLog = {
+        id: this.id,
+        text: this.text,
+        date: new Date()
+      };
+      // update log
+    this.logService.updateLog(updLog);
+    }
+  }
+
+  generateId() {
+  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g,
+    function(c) {
+      const r = Math.random() * 16 | 0, v = c === 'x' ? r : (r & 0x3 | 0x8);
+      return v.toString(16);
+    });
+  }
+
 }
